376c87a3c528c1e70d28aabe7a2dbeee29e60b86
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-06-10 12:50:31 +0200

    Add example database configuration file and update .gitignore to exclude it

---
+++ config/database.example.php
@@ -0,0 +1,7 @@
+<?php
+# ersetze diese Info mit neue script database.php mit dein Info
+define('DB_HOST', 'DEIN_HOST');
+define('DB_NAME', 'DEIN_DATABASE');
+define('DB_USER', 'USER');
+define('DB_PASS', 'PASSWORD');
+?>
