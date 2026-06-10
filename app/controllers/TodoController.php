73bde5874b81c1def83d91643afe088a392865bf
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-06-10 11:41:36 +0200

    Add TodoController with CRUD operations for todos

---
+++ app/controllers/TodoController.php
@@ -0,0 +1,54 @@
+<?php
+#declare(strict_types=1);
+
+class TodoController
+{
+    private Todo $model;
+
+    public function __construct()
+    {
+        $this->model = new Todo();
+    }
+
+    // GET /todos
+    public function index(): void
+    {
+        Response::json($this->model->all());
+    }
+
+    // GET /todos/{id}
+    public function show(?string $id): void
+    {
+        $todo = $this->model->find((int) $id);
+        $todo ? Response::json($todo) : Response::json(['error' => 'Not found'], 404);
+    }
+
+    // POST /todos
+    public function store(): void
+    {
+        $data = json_decode(file_get_contents('php://input'), true);//php:// Stream Wrappers
+        
+
+        if (empty($data['title'])) {
+            Response::json(['error' => 'Title ist Pflicht'], 422);
+        }
+
+        $id = $this->model->create($data);
+        Response::json(['message' => 'Erstellt', 'id' => $id], 201);
+    }
+
+    // PUT /todos/{id}
+    public function update(?string $id): void
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $this->model->update((int) $id, $data);
+        Response::json(['message' => 'Aktualisiert']);
+    }
+
+    // DELETE /todos/{id}
+    public function destroy(?string $id): void
+    {
+        $this->model->delete((int) $id);
+        Response::json(['message' => 'Gelöscht (Soft Delete)']);
+    }
+}
